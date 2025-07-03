54b3720fa3bcfdbe0cecc24c8f2dd47ef8a2b039
cedric <demuelenaere.cedric@gmail.com>
2025-07-03 14:14:39 +0200

    3 scripts merged

--- level_generator/2_prepare_levels.py
+++ level_generator/2_prepare_levels.py
@@ -1,5 +1,84 @@
+import numpy
 import pickle
+import statistics
+import matplotlib.pyplot as plt
 from config import *
+import pickle
+import json
+
+def displayAll(L):
+    plt.title("All evolution")
+    for elem in L:
+        plt.plot(elem.historyOfScores)
+    plt.show()
+
+def describeList(L):
+    print("min : ",min(L))
+    print("10% low",numpy.percentile(L, 10))
+    print("med",statistics.median(L))
+    print("10% high",numpy.percentile(L, 90))
+    print("max : ",max(L))
+
+
+def describeBunchOfLevels(prefixes_list, quantity):
+    for prefix in prefixes_list:
+
+        print('----> Current prefix', prefix)
+        complete_levels_list = []
+
+        ##open all the files, put in a list
+        for current_level_index in range(quantity):
+            file = open(prefix + str(current_level_index), 'rb')
+            elem = pickle.load(file)
+            complete_levels_list.append(elem)
+
+        print("\n==============> Nb of levels :  ", len(complete_levels_list), " levels")
+
+        # =========================================================================== get stats
+        scores = []
+        sizes = []
+        fitness = []
+
+        ##open all them files, put in a list
+        for data in complete_levels_list:
+            data.setFitnessScore()
+
+            scores.append(data.best_score)
+            sizes.append(len(data.best_moves))
+            fitness.append(data.fitness)
+
+        # =========================================================================== Describe stats in terminal
+
+        print('\n==============> Scores')
+        describeList(scores)
+
+        print('\n==============> Sizes')
+        describeList(sizes)
+
+        print('\n==============> Fitness')
+        describeList(fitness)
+
+        # =========================================================================== Display stats as plots
+
+        displayAll(complete_levels_list)
+
+        scores.sort()
+        fitness.sort()
+        sizes.sort()
+
+        plt.title("All final scores")
+        plt.plot(scores)
+        plt.show()
+
+        plt.title("All final fitness")
+        plt.plot(fitness)
+        plt.show()
+
+        plt.title("All final sizes")
+        plt.plot(sizes)
+        plt.show()
+
+
 
 def createLevelFile(level, filename):
     temp=open(filename,"wb")
@@ -114,4 +193,33 @@ def reduceLevelsSet():
 
         print("\n==============>  Keeping ", len(final_levels_list), " levels")
 
+def createLevelFileAsJson(l, filename):
+    result={
+        "operations":l.level.level,
+        "bestScore":round(float(l.best_score),2),
+        "bestMoves":l.best_moves
+    }
+
+    with open(filename, 'w') as file:
+        json.dump(result, file, indent=4, separators=(',', ': '), ensure_ascii=False)
+
+def exportAllLevelsAsJson():
+    for difficulty in difficulties:
+        print('====> Current difficulty', difficulty)
+
+        ##open all the files, put in a list
+        for final_index_level in range(number_levels_to_keep):
+            file = open(file_prefixes_processed[difficulty] + str(final_index_level), 'rb')
+            data = pickle.load(file)
+
+            createLevelFileAsJson(data, file_prefixes_processed_as_json[difficulty] + str(final_index_level) + ".json")
+
+
+print("==> step 1")
+describeBunchOfLevels(file_prefixes_raw, raw_levels_to_generate)
+print("==> step 2")
 reduceLevelsSet()
+print("==> step 3")
+describeBunchOfLevels(file_prefixes_processed, number_levels_to_keep)
+print("==> step 4")
+exportAllLevelsAsJson()
