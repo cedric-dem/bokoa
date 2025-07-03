634deaade953f30018c16230e80b99eb325c4c24
cedric <demuelenaere.cedric@gmail.com>
2025-07-03 23:41:28 +0200

    refactor of historyOfScores (split into function then go through history)

--- level_generator/levelWithSolution.py
+++ level_generator/levelWithSolution.py
@@ -1,73 +1,87 @@
 from game import Game
 from config import *
 
+
+def get_history_of_scores_for_given_solution_on_given_level(solution, level):
+    current_game = Game(level)
+
+    historyOfScoresForBestSolution = [1]
+
+    for move in solution:
+        move_direction = get_direction(move)
+        current_position = current_game.position_history[-1]
+        new_position = [current_position[0] + move_direction[0], current_position[1] + move_direction[1]]
+        current_game.move(move_direction, new_position)
+
+        historyOfScoresForBestSolution.append(current_game.score)
+
+    return historyOfScoresForBestSolution
+
+
 def get_direction(move):
-    if move==">": #good
-        result=[0,1]
-        
-    elif move=="<":
-        result=[0,-1]
-        
-    elif move=="u":
-        result=[1,0]
-    
-    elif move=="n":
-        result=[-1,0]
-    
+    if move == ">":  # good
+        result = [0, 1]
+
+    elif move == "<":
+        result = [0, -1]
+
+    elif move == "u":
+        result = [1, 0]
+
+    elif move == "n":
+        result = [-1, 0]
+
     return result
-    
+
+
 class LevelWithSolution(object):
-    def __init__(self,level,best_score,best_moves):
-        self.level=level
+    def __init__(self, level, best_score, best_moves):
+        self.level = level
 
-        self.best_score=best_score
-        self.best_moves=best_moves
+        self.best_score = best_score
+        self.best_moves = best_moves
 
-        self.historyOfScoresForBestSolution=None
+        self.historyOfScoresForBestSolution = None
 
-        self.grid_size_id=level.grid_size_id
-        self.grid_size=level.grid_size
-    
+        self.grid_size_id = level.grid_size_id
+        self.grid_size = level.grid_size
+
     def set_fitness_score(self):
-        self.historyOfScoresForBestSolution=[1]
-        current_game=Game(self.level)
+        self.historyOfScoresForBestSolution = get_history_of_scores_for_given_solution_on_given_level(self.best_moves,
+                                                                                                      self.level)
 
-        increasing_steps_counter=0
+        increasing_steps_counter = 0
 
-        total_score_decreasing=0
-        total_score_increasing=0
-        
-        for move in self.best_moves:
-            old_score=current_game.score
+        total_score_decreasing = 0
+        total_score_increasing = 0
 
-            move_direction=get_direction(move)
-            current_position=current_game.position_history[-1]
-            new_position=[current_position[0]+move_direction[0], current_position[1]+move_direction[1]]
-            current_game.move(move_direction,new_position)
-            
-            self.historyOfScoresForBestSolution.append(current_game.score)
-            new_score=current_game.score
+        for current_score_index in range(1, len(self.historyOfScoresForBestSolution)):
+            old_score = self.historyOfScoresForBestSolution[current_score_index - 1]
+            new_score = self.historyOfScoresForBestSolution[current_score_index]
 
-            if new_score>old_score:
-                increasing_steps_counter+=1
-                total_score_increasing+=(new_score-old_score)
+            if new_score > old_score:
+                increasing_steps_counter += 1
+                total_score_increasing += (new_score - old_score)
 
-            if new_score<old_score:
-                total_score_decreasing+=(old_score-new_score)
+            if new_score < old_score:
+                total_score_decreasing += (old_score - new_score)
 
-        fitness_first_term = coefficient_fitness_first_term_a[self.grid_size_id] - (coefficient_fitness_first_term_b[self.grid_size_id] * (increasing_steps_counter / (len(self.historyOfScoresForBestSolution))))
-        fitness_second_term = (total_score_decreasing / self.historyOfScoresForBestSolution[-1]) / coefficient_fitness_second_term_a[self.grid_size_id]
+        fitness_first_term = coefficient_fitness_first_term_a[self.grid_size_id] - (
+                    coefficient_fitness_first_term_b[self.grid_size_id] * (
+                        increasing_steps_counter / (len(self.historyOfScoresForBestSolution))))
+        fitness_second_term = (total_score_decreasing / self.historyOfScoresForBestSolution[-1]) / \
+                              coefficient_fitness_second_term_a[self.grid_size_id]
 
-        self.estimated_difficulty= (coefficient_fitness_second_term * fitness_second_term) + fitness_first_term
+        self.estimated_difficulty = (coefficient_fitness_second_term * fitness_second_term) + fitness_first_term
 
     def display_everything(self):
         print('==> Grid :')
         self.level.display_level()
-        print('==> Solution :',self.best_moves)
-        print('==> Best Score : ',self.best_score)
+        print('==> Solution :', self.best_moves)
+        print('==> Best Score : ', self.best_score)
 
-    def __gt__(self,other):
-        return self.estimated_difficulty>other.estimated_difficulty
+    def __gt__(self, other):
+        return self.estimated_difficulty > other.estimated_difficulty
 
-    def __eq__(self,other):
-        return self.estimated_difficulty==other.estimated_difficulty
+    def __eq__(self, other):
+        return self.estimated_difficulty == other.estimated_difficulty
