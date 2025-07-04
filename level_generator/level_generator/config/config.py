11759797ae916e2906518727de552e6ae0cea2bf
cedric <demuelenaere.cedric@gmail.com>
2025-07-04 15:50:59 +0200

    add new computed coefficients to config to compare both situations

--- level_generator/level_generator/config/config.py
+++ level_generator/level_generator/config/config.py
@@ -13,9 +13,15 @@ display_new_levels = True
 coefficient_difficulty_first_term = 0.4
 coefficient_difficulty_second_term = 0.6
 
-coefficient_difficulty_first_term_a = [2.0998, 2.332, 1.92]
-coefficient_difficulty_first_term_b = [2.34, 2.666, 2.026]
-coefficient_difficulty_second_term_a = [0.826446281, 1.597444089, 1.34589502]
+# OLD
+#coefficient_difficulty_first_term_a = [2.0998, 2.332, 1.92]
+#coefficient_difficulty_first_term_b = [2.34, 2.666, 2.026]
+#coefficient_difficulty_second_term_a = [0.826446281, 1.597444089, 1.34589502]
+
+# NEW
+coefficient_difficulty_first_term_a = [2.076923076923077, 2.2 , 1.9109589041095891 ]
+coefficient_difficulty_first_term_b = [2.3076923076923075,2.4000000000000004 , 2.017123287671233  ]
+coefficient_difficulty_second_term_a = [0.8157894736842106,1.5814696485623003, 1.3325587613008851]
 
 generated_levels_folder_name = "generated_levels_published"
 
