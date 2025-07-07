97cfc87911241b80923ef10b5aa42dd48ccf6532
cedric <demuelenaere.cedric@gmail.com>
2025-07-08 01:14:42 +0200

    added optimization : matrix_of_occupation instead of history of positions, avoid to loop on lost of positions

--- level_generator/level_generator/classes/game.py
+++ level_generator/level_generator/classes/game.py
@@ -5,15 +5,19 @@ class Game(object):
 		self.grid_size = level.grid_size
 
 		self.moves_history = []
-		self.position_history = [[0, 0]]
+
+		self.current_position_head = [0, 0]
+
+		self.occupation_matrix = [[False for _ in range(self.grid_size[0])] for _ in range(self.grid_size[1])]
+		self.occupation_matrix[0][0] = True
 
 	def apply_move_given_direction(self, direction):
-		new_pos = [self.position_history[-1][0] + direction[0], self.position_history[-1][1] + direction[1]]
+		new_pos = [self.current_position_head[0] + direction[0], self.current_position_head[1] + direction[1]]
 
 		self.apply_move_given_direction_and_new_pos(direction, new_pos)
 
 	def apply_move_given_direction_and_new_pos(self, direction, new_pos):
-		#For optimization purposes, back_track can call directly this function given new pos, saves like 5% of time not to recompute new_pos
+		# For optimization purposes, back_track can call directly this function given new pos, saves like 5% of time not to recompute new_pos
 
 		self.moves_history.append(direction)
 
@@ -21,7 +25,8 @@ class Game(object):
 
 		self.apply_operation(new_operation)
 
-		self.position_history.append(new_pos)
+		self.occupation_matrix[new_pos[0]][new_pos[1]] = True
+		self.current_position_head = new_pos
 
 	def apply_operation(self, operation):
 		match operation[0]:
@@ -40,4 +45,4 @@ class Game(object):
 		return new_pos[0] >= 0 and new_pos[1] >= 0 and new_pos[0] < self.grid_size[1] and new_pos[1] < self.grid_size[0]
 
 	def is_move_in_history(self, new_pos):
-		return new_pos in self.position_history
+		return self.occupation_matrix[new_pos[0]][new_pos[1]]
--- level_generator/level_generator/utils/misc_functions.py
+++ level_generator/level_generator/utils/misc_functions.py
@@ -113,12 +113,13 @@ def back_track(game, max_solution_size):
 	if len(game.moves_history) < max_solution_size:  # else stop
 		for new_move in get_all_but_inverse_of_last_move(game.moves_history):
 
-			new_position = [game.position_history[-1][0] + new_move[0], game.position_history[-1][1] + new_move[1]]
+			new_position = [game.current_position_head[0] + new_move[0], game.current_position_head[1] + new_move[1]]
 
 			##if move ok + not coming back
 			if game.is_move_in_bound(new_position) and (not game.is_move_in_history(new_position)):
 				# save old score
 				old_score = game.score
+				old_head_position = game.current_position_head
 
 				# move
 				game.apply_move_given_direction_and_new_pos(new_move, new_position)
@@ -129,7 +130,10 @@ def back_track(game, max_solution_size):
 				##restore old state
 				game.score = old_score
 				game.moves_history.pop()
-				game.position_history.pop()
+				game.current_position_head = old_head_position
+
+				# game.occupation_matrix[old_head_position[0]][old_head_position[1]] = False
+				game.occupation_matrix[new_position[0]][new_position[1]] = False
 
 				if current_best_score < temp_best_score:  # if new res better than prev:
 					current_best_score = temp_best_score
