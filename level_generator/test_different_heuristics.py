256740f7d0eee6f1157c3678b198f10efcab4e68
cedric <demuelenaere.cedric@gmail.com>
2025-07-09 15:24:07 +0200

    start score check heuristic + execute only current heuristic for now

--- level_generator/test_different_heuristics.py
+++ level_generator/test_different_heuristics.py
@@ -3,10 +3,10 @@ from level_generator.utils.reduce_levels_functions import get_complete_set_level
 
 print('=====> Test Proportion of levels passed by each heuristics : ')
 
-#initial_set_of_levels = get_complete_set_levels()
+# initial_set_of_levels = get_complete_set_levels()
 reduced_set_of_levels = get_reduced_set_levels()
 
-evaluate_heuristic_performance("Greedy", reduced_set_of_levels)
-evaluate_heuristic_performance("Advantage Matrix", reduced_set_of_levels)
-evaluate_heuristic_performance("BackTracking Limited Depth", reduced_set_of_levels)
+# evaluate_heuristic_performance("Greedy", reduced_set_of_levels)
+# evaluate_heuristic_performance("Advantage Matrix", reduced_set_of_levels)
+# evaluate_heuristic_performance("BackTracking Limited Depth", reduced_set_of_levels)
 evaluate_heuristic_performance("BackTracking With Score Check", reduced_set_of_levels)
