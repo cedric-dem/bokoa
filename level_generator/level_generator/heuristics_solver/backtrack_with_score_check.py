671ecf01b291fbe7524fae9c9ec334561c1fcb5d
cedric <demuelenaere.cedric@gmail.com>
2025-07-09 18:02:26 +0200

    experimenting with score check heuristic

--- level_generator/level_generator/heuristics_solver/backtrack_with_score_check.py
+++ level_generator/level_generator/heuristics_solver/backtrack_with_score_check.py
@@ -27,4 +27,10 @@ class BackTrackingWithScoreCheckSolver(Solver):
 
 	def is_solution_worth_trying(self, current_score, current_depth):
 		estimated_lower = get_approx_function(self.level_to_solve.grid_size_id)[current_depth]
-		return current_score >= estimated_lower + (current_depth * 1.2)  # add margin
+
+		# lower the margin => better accuracy, but cost more time
+
+		margin = (current_depth * 2) - 1  # starts at 0, increase as depth goes on (really fast,1.2 seconds, and 84% accuracy)
+		# margin =  (current_depth * 1.2)  # starts at 0, increase as depth goes on (really fast, still 93% accuracy approximately, takes 2 seconds for grid size 6x6)
+		# margin = -1  # slower, roughly 9 sec for grid size 6x6 but 100% reliable on reduced set
+		return current_score >= estimated_lower + margin
