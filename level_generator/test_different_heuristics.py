8081ce26ea463d6bc11813aeac4eb2b14c2f9ada
cedric <demuelenaere.cedric@gmail.com>
2025-07-09 21:25:25 +0200

    use tiny level set for heuristic test files

--- level_generator/test_different_heuristics.py
+++ level_generator/test_different_heuristics.py
@@ -3,9 +3,9 @@ from level_generator.utils.reduce_levels_functions import get_complete_set_level
 
 print('=====> Test Proportion of levels passed by each heuristics : ')
 
-# set_of_levels = get_complete_set_levels()
-# set_of_levels = get_reduced_set_levels()
-set_of_levels = [levels_for_that_grid_size[:10] for levels_for_that_grid_size in get_reduced_set_levels()]
+#set_of_levels = get_complete_set_levels()
+set_of_levels = get_reduced_set_levels()
+# set_of_levels = [levels_for_that_grid_size[:10] for levels_for_that_grid_size in get_reduced_set_levels()]
 
 # evaluate_heuristic_performance("Greedy", set_of_levels)
 evaluate_heuristic_performance("Advantage Matrix", set_of_levels)
