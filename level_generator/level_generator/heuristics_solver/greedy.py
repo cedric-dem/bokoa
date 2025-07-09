e9a8ef90d7acbc1bf14df9d4119cc96413f120e9
cedric <demuelenaere.cedric@gmail.com>
2025-07-09 18:32:16 +0200

    clean code

--- level_generator/level_generator/heuristics_solver/greedy.py
+++ level_generator/level_generator/heuristics_solver/greedy.py
@@ -25,14 +25,16 @@ class GreedySolver(Solver):
 		return result
 
 	def is_solution_worth_trying(self, current_score, current_depth, new_operation):
-		point_change = int(grid_sizes[self.level_to_solve.grid_size_id][0] * grid_sizes[self.level_to_solve.grid_size_id][1] / 2)
+		half_max_length = int(grid_sizes[self.level_to_solve.grid_size_id][0] * grid_sizes[self.level_to_solve.grid_size_id][1] / 2)
+		early_point= int(grid_sizes[self.level_to_solve.grid_size_id][0] * grid_sizes[self.level_to_solve.grid_size_id][1] / 4)
+
+		is_before_half_max_length = current_depth > half_max_length
 
 		is_positive = (new_operation.operation == "+" or new_operation.operation == "×")
-		is_divide_and_late = (current_depth > point_change and new_operation.operation == "÷")
-		is_minus_and_early = (current_depth < point_change and new_operation.operation == "-")
-		is_early = current_depth > point_change
+		is_divide_and_late = (is_before_half_max_length and new_operation.operation == "÷")
+		is_minus_and_early = ((not is_before_half_max_length) and new_operation.operation == "-")
 
 		# result = is_positive or (is_divide_and_late or is_minus_and_early) # Absolute greed, not reliable but quite instantaneous
-		result = is_positive or is_divide_and_late or is_minus_and_early or is_early  # slightly better, still instantaneous
+		result = is_positive or is_divide_and_late or is_minus_and_early or is_before_half_max_length  # slightly better, still instantaneous
 
 		return result
