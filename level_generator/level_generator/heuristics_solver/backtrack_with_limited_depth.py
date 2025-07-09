8e8d51725a9129176fd59c1b13a5891a4e449ea6
cedric <demuelenaere.cedric@gmail.com>
2025-07-09 18:06:55 +0200

    change max depth heuristic

--- level_generator/level_generator/heuristics_solver/backtrack_with_limited_depth.py
+++ level_generator/level_generator/heuristics_solver/backtrack_with_limited_depth.py
@@ -25,5 +25,5 @@ class BackTrackingLimitedDepthSolver(Solver):
 		return result
 
 	def is_solution_worth_trying(self, current_score, current_depth):
-		max_depth = int(grid_sizes[self.level_to_solve.grid_size_id][0] * grid_sizes[self.level_to_solve.grid_size_id][1] * 0.4)  # TODO fine tune, find best tradeoff than this
+		max_depth = int(grid_sizes[self.level_to_solve.grid_size_id][0] * grid_sizes[self.level_to_solve.grid_size_id][1] * 0.5)  # TODO fine tune, find best tradeoff than this
 		return  current_depth < max_depth
