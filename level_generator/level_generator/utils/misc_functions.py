a1e413f5bf0161915403a1896c038e368987c501
cedric <demuelenaere.cedric@gmail.com>
2025-07-11 13:50:32 +0200

    fix back_tracking not working since heuristic added

--- level_generator/level_generator/utils/misc_functions.py
+++ level_generator/level_generator/utils/misc_functions.py
@@ -116,7 +116,7 @@ def back_track(game, max_solution_size, heuristic):
 			new_position = [game.current_position_head[0] + new_move[0], game.current_position_head[1] + new_move[1]]
 
 			##if move ok + not coming back
-			if game.is_move_in_bound_and_not_in_history(new_position) and (heuristic and heuristic.is_solution_worth_trying(game, new_position)):
+			if game.is_move_in_bound_and_not_in_history(new_position) and (heuristic is None or heuristic.is_solution_worth_trying(game, new_position)):
 				# save old score
 				old_score = game.score
 				old_head_position = game.current_position_head
