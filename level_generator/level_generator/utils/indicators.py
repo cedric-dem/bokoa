49093685ce535eb58633e8a5f28b4728b8cb4c8b
cedric <demuelenaere.cedric@gmail.com>
2025-07-14 16:59:30 +0200

    clean code

--- level_generator/level_generator/utils/indicators.py
+++ level_generator/level_generator/utils/indicators.py
@@ -1,31 +1,37 @@
 from level_generator.utils.level_with_sol_creation_functions import get_history_of_operations_for_given_solution_on_given_level, get_occupation_matrix_for_given_solution_on_given_level
 
 def get_all_indicators(level):
-	score_indicators = get_score_indicators(level)
-	operations_used_indicator = get_operations_used_indicator(level)
-	remaining_operations_indicator = get_remaining_operations_indicator(level)
+	##########################################################################################################
+	increasing_steps_counter = 0
 
-	return score_indicators + [operations_used_indicator] + [remaining_operations_indicator]
+	total_score_decreasing = 0
+	total_score_increasing = 0
 
-def get_remaining_operations_indicator(level):
-	# todo remove code duplication
-	occupation_matrix = get_occupation_matrix_for_given_solution_on_given_level(level.best_moves, level)
+	latest_negative_score_at = 0
 
-	current_indicator_value = 0
+	for current_score_index in range(1, len(level.history_of_scores_for_best_solution)):
+		old_score = level.history_of_scores_for_best_solution[current_score_index - 1]
+		new_score = level.history_of_scores_for_best_solution[current_score_index]
 
-	for i in range(len(occupation_matrix)):
-		for j in range(len(occupation_matrix[i])):
-			if not occupation_matrix[i][j]:
-				this_unused_operation = level.operations_grid[i][j]
-				if this_unused_operation.operation == "+":
-					current_indicator_value += 3 + (2 * this_unused_operation.operand)
+		if new_score > old_score:
+			increasing_steps_counter += 1
+			total_score_increasing += (new_score - old_score)
 
-				elif this_unused_operation.operation == "×":
-					current_indicator_value += 5 + (3 * this_unused_operation.operand)
+		if new_score < old_score:
+			total_score_decreasing += (old_score - new_score)
 
-	return current_indicator_value
+		if new_score < 0:
+			latest_negative_score_at = current_score_index
 
-def get_operations_used_indicator(level):
+	# TODO try with srqt or squared
+	proportion_increasing_steps = -increasing_steps_counter / (len(level.history_of_scores_for_best_solution))
+	proportion_score_decreasing = (total_score_decreasing / level.history_of_scores_for_best_solution[-1])
+
+	lowest_score = - min(level.history_of_scores_for_best_solution)
+
+	solution_length = len(level.history_of_scores_for_best_solution)
+
+	##########################################################################################################
 	operations = get_history_of_operations_for_given_solution_on_given_level(level.best_moves, level)
 	occupation = get_occupation_matrix_for_given_solution_on_given_level(level.best_moves, level)
 	scores_history = level.history_of_scores_for_best_solution
@@ -62,36 +68,23 @@ def get_operations_used_indicator(level):
 			case _:
 				raise ValueError("Invalid Value  (Operation not found) : ", current_operation.operation)
 
-	return -round(current_difficulty_estimate, 2)
+	operations_used_indicator = -round(current_difficulty_estimate, 2)
 
-def get_score_indicators(level):
-	increasing_steps_counter = 0
+	##########################################################################################################
+	# todo remove code duplication
+	occupation_matrix = get_occupation_matrix_for_given_solution_on_given_level(level.best_moves, level)
 
-	total_score_decreasing = 0
-	total_score_increasing = 0
+	current_indicator_value = 0
 
-	latest_negative_score_at = 0
+	for i in range(len(occupation_matrix)):
+		for j in range(len(occupation_matrix[i])):
+			if not occupation_matrix[i][j]:
+				this_unused_operation = level.operations_grid[i][j]
+				if this_unused_operation.operation == "+":
+					current_indicator_value += 3 + (2 * this_unused_operation.operand)
 
-	for current_score_index in range(1, len(level.history_of_scores_for_best_solution)):
-		old_score = level.history_of_scores_for_best_solution[current_score_index - 1]
-		new_score = level.history_of_scores_for_best_solution[current_score_index]
+				elif this_unused_operation.operation == "×":
+					current_indicator_value += 5 + (3 * this_unused_operation.operand)
+	remaining_operations_indicator = current_indicator_value
 
-		if new_score > old_score:
-			increasing_steps_counter += 1
-			total_score_increasing += (new_score - old_score)
-
-		if new_score < old_score:
-			total_score_decreasing += (old_score - new_score)
-
-		if new_score < 0:
-			latest_negative_score_at = current_score_index
-
-	# TODO try with srqt or squared
-	proportion_increasing_steps = -increasing_steps_counter / (len(level.history_of_scores_for_best_solution))
-	proportion_score_decreasing = (total_score_decreasing / level.history_of_scores_for_best_solution[-1])
-
-	lowest_score =  - min(level.history_of_scores_for_best_solution)
-
-	solution_length = len(level.history_of_scores_for_best_solution)
-
-	return [proportion_increasing_steps, proportion_score_decreasing, lowest_score, solution_length, latest_negative_score_at]
+	return [proportion_increasing_steps, proportion_score_decreasing, lowest_score, solution_length, latest_negative_score_at, operations_used_indicator, remaining_operations_indicator]
