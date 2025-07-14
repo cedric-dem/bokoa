3b28075742dd09bed033f004bcdd7f8374bdc409
cedric <demuelenaere.cedric@gmail.com>
2025-07-14 14:24:59 +0200

    clean code

--- level_generator/level_generator/utils/constant_computation.py
+++ level_generator/level_generator/utils/constant_computation.py
@@ -9,16 +9,18 @@ def get_coef_affine(min_value, max_value):
 def retrieve_all_constants(set_of_levels):
 	match compute_constants:
 		case "AUTOMATIC":
-			t1, t2, t3, t4 = [], [], [], []
+
+			result = [[[], []] for _ in range(len(weights_parameters))]
+
 			for grid_size_id in range(len(grid_sizes)):
 				print('=' * 190)
 				print('====> Retrieving Automatically constants  grid size :', grid_sizes[grid_size_id])
-				[tt1, tt2], [tt3, tt4] = retrieve_constants_automatically(set_of_levels[grid_size_id])
 
-				t1.append(tt1)
-				t2.append(tt2)
-				t3.append(tt3)
-				t4.append(tt4)
+				current_grid_size_constants = retrieve_constants_automatically(set_of_levels[grid_size_id])
+
+				for current_constant_index in range(len(current_grid_size_constants)):
+					result[current_constant_index][0].append(current_grid_size_constants[current_constant_index][0])
+					result[current_constant_index][1].append(current_grid_size_constants[current_constant_index][1])
 
 		case "USE_OLD":
 			print('====> Retrieving HardCoded old constants ')
@@ -26,6 +28,7 @@ def retrieve_all_constants(set_of_levels):
 			t2 = [2.34, 2.666, 2.026]
 			t3 = [0.826446281, 1.597444089, 1.34589502]
 			t4 = [1.00, 0.00, 0.00]
+			result = []
 
 		case "USE_NEW":
 			print('====> Retrieving HardCoded new constants ')
@@ -33,14 +36,12 @@ def retrieve_all_constants(set_of_levels):
 			t2 = [2.3076923076923075, 2.4000000000000004, 2.017123287671233]
 			t3 = [0.8157894736842106, 1.5814696485623003, 1.3325587613008851]
 			t4 = [1.00, 0.00, 0.00]
+			result = []
 
 		case _:
 			raise ValueError("Invalid compute constant method : ", compute_constants)
 
-	return [
-		[t1, t2],
-		[t3, t4]
-	]
+	return result
 
 def retrieve_constants_automatically(complete_levels_list):
 	# ==== get stats
