7ef51c4419591d427ab1b67fc7f504b003d9b429
cedric <demuelenaere.cedric@gmail.com>
2025-07-14 15:25:13 +0200

    create config file for published levels

---
+++ level_generator/level_generator/config/config_old.py
@@ -0,0 +1,41 @@
+############################################# Common
+grid_sizes = [[4, 4], [5, 5], [6, 6]]
+
+format_json = True
+
+generated_levels_folder_name = "generated_levels/generated_levels_published"
+
+complete_folder_name = "complete"
+reduced_folder_name = "reduced"
+
+grid_size_folder_prefix = "grid_size_"
+
+level_file_name = "level_"
+
+############################################# Generate Levels
+raw_levels_to_generate = 1000
+
+balance_operand = True
+
+use_multiple_cores_for_levels_generation = True
+n_cores = 6
+
+############################################# Prepare Levels
+number_levels_to_keep = 100
+
+ignore_extreme_values = 10  # will ignore top 1%, bottom 1% (scores and sizes)
+
+weights_parameters = [
+	0.4,  # proportion_increasing_steps,
+	0.6,  # proportion_score_decreasing
+	0,  # lowest_score
+	0,  # solution_length
+	0,  # latest_negative_score_at
+	0,  # current_difficulty_estimate
+	0  # remaining_operations
+]
+
+compute_constants = "USE_OLD"
+compute_boundaries = "USE_OLD"
+
+difficulty_setting = "linear"
