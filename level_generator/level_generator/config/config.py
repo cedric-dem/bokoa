ac491876d055f4770ee19fd7852c5fa387344624
cedric <demuelenaere.cedric@gmail.com>
2025-07-14 15:23:00 +0200

    remove unused config

--- level_generator/level_generator/config/config.py
+++ level_generator/level_generator/config/config.py
@@ -40,4 +40,3 @@ compute_constants = "AUTOMATIC"
 compute_boundaries = "AUTOMATIC"
 
 difficulty_setting = "linear"
-difficulty_function = "sum_two_terms"  # "points_estimate" "sum_two_terms" "hardcoded_constants_sum_two_terms"
